9495ac0c542b7f7161a468776e09a026108a7d8c
eumunyana <eumunyana@alustudent.com>
2024-09-03 17:42:03 +0000

    2. Basic annotations - floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""a script for a type-annotated function floor which takes
+"""A script for a type-annotated function floor which takes
 a float n as argument and returns the floor of the float."""
 
 import math
@@ -8,7 +8,7 @@ import math
 def floor (n: float)-> int:
 	"""
 	Args:
-		n (float): float
+		n(float): float
 
 	Returns:
 		floar: int
