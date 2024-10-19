a7bfbe7aed6abd7af37400a2c977639dd141b53b
esther umunyana <116874050+eumunyana@users.noreply.github.com>
2024-10-19 04:48:58 +0200

    Update 4-app.py

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,37 +1,46 @@
 #!/usr/bin/env python3
-"""Basic Flask app with internationalization support"""
-from flask_babel import Babel
+"""
+Flask app
+"""
 from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
 class Config:
-    """Flask Babel configuration"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
 app.url_map.strict_slashes = False
+app.config.from_object(Config)
+
 babel = Babel(app)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """ GET /
+    Return:
+      - Render template
+    """
+    return render_template('4-index.html')
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page"""
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+def get_locale():
+    """
+    Get locale from request
+    """
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-@app.route('/')
-def get_index() -> str:
-    """index page"""
-    return render_template('4-index.html')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
