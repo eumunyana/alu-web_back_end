d582f480661105cf0929c0c174f3cc3c0934168c
eumunyana <eumunyana@alustudent.com>
2024-11-02 17:23:44 +0000

    Task 1. Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
- export default function taskBlock(trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars, no-shadow
-    const task = true;
-    // eslint-disable-next-line no-unused-vars, no-shadow
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
- }
+}
